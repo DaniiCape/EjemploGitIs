a663a76e724d636fcd89e22d997fc8aa9efc1494
jonathan <jobaji09@gmail.com>
2018-02-24 23:18:21 -0600

    Identa por favor

--- holamundo.java
+++ holamundo.java
@@ -2,8 +2,9 @@ public class holamundo {
 
 
 public static void main (String [] args ){
-System.out.print("Que pedooo, no me acuerdo de java bye \n");
-System.out.println("No problema yo te ayudo");
+    System.out.print("Que pedooo, no me acuerdo de java bye \n");
+    System.out.println("No problema yo te ayudo");
+    
 }
 
 
