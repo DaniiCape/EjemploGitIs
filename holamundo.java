f0620738dfbe1aeefeb3abb85eb2d76957b9c8b5
jonathan <jobaji09@gmail.com>
2018-02-24 23:13:42 -0600

    Las clases no van con minusculas

--- holamundo.java
+++ holamundo.java
@@ -3,7 +3,7 @@ public class holamundo {
 
 public static void main (String [] args ){
 System.out.print("Que pedooo, no me acuerdo de java bye \n");
-
+System.out.println("No problema yo te ayudo");
 }
 
 
