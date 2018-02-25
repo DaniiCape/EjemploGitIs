cb1b03d6ea3becdec8c4b17f0d91f0e6addee288
Dany <daniicape19@gmail.com>
2018-02-24 23:26:36 -0600

    Modificando clases

--- holamundo.java
+++ holamundo.java
@@ -4,6 +4,7 @@ public class holamundo {
 public static void main (String [] args ){
     System.out.print("Que pedooo, no me acuerdo de java bye \n");
     System.out.println("No problema yo te ayudo");
+    System.out.println("Vamo' a tronar esta cosa'");
     
 }
 
