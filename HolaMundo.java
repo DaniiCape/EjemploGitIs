11bc3235d670408d8f2dd66fd6924232d9911c11
Dany <daniicape19@gmail.com>
2018-02-24 23:41:12 -0600

    Mas cambios

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,12 +1,12 @@
 public class HolaMundo {
 
 
-<<<<<<< HEAD
+
 public static void main (String [] args ){
-System.out.print("Que pedooo, no me acuerdo de java bye \n");
-System.out.println("No problema yo te ayudo");
-System.out.println("YO me encargo de esa parte");
-System.out.println("tronar, tronar y trona a ar");
+    System.out.print("Que pedooo, no me acuerdo de java bye \n");
+    System.out.println("No problema yo te ayudo");
+    System.out.println("YO me encargo de esa parte");
+    System.out.println("tronar, tronar y trona a ar");
 }
 
 
